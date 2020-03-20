561155e660866dd56015043a307c554edffb340f
Zoe Stokes <sekotszs@gmail.com>
2020-03-20 09:05:08 -0700

    got the data

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from "axios";
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,6 +10,21 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const [data, setData] = useState();
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  const url = 'https://swapi.co/api/people';
+
+  useEffect(() =>{
+    axios.get(proxy+url)
+    .then(response =>{
+      console.log(response);
+      // setData(response.data);
+    })
+    .catch(error => {console.log('Where is the data', error)
+  })
+  }, [])
+
 
   return (
     <div className="App">
